2098c8d53a9a08ba08d4425c15bf4253eea24469
kw <k.wojtasinski@wp.pl>
2022-02-13 21:23:24 +0100

    Added controllers

---
+++ src/Modules/Contacts/ECommerce.Modules.Contacts.Api/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Shared.Infrastructure.Api;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Modules.Contacts.Api.Controllers
+{
+    [ApiController]
+    [ProducesDefaultContentType]
+    [Route(ContactsModule.BasePath + "/[controller]")]
+    internal class BaseController
+    {
+    }
+}
---
+++ src/Modules/Contacts/ECommerce.Modules.Contacts.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Modules.Contacts.Api.Controllers
+{
+    [Route(ContactsModule.BasePath)]
+    internal class HealthCheckController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Contacts API";
+    }
+}
