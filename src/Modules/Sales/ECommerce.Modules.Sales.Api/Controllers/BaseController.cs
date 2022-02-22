6d95bec7bfdf9b1bdc65452c22a730be214f1397
kw <k.wojtasinski@wp.pl>
2022-02-22 19:42:03 +0100

    Added base controller to sales module

---
+++ src/Modules/Sales/ECommerce.Modules.Sales.Api/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Shared.Infrastructure.Api;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Modules.Sales.Api.Controllers
+{
+    [ApiController]
+    [Route(SalesModule.BasePath + "/[controller]")]
+    [ProducesDefaultContentType]
+    internal class BaseController : ControllerBase
+    {
+        protected ActionResult<T> OkOrNotFound<T>(T model)
+        {
+            if (model is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        protected void AddResourceIdHeader(Guid id) => Response.Headers.Add("Resource-ID", id.ToString());
+    }
+}
---
+++ src/Modules/Sales/ECommerce.Modules.Sales.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Modules.Sales.Api.Controllers
+{
+    [Route(SalesModule.BasePath)]
+    internal class HealthCheckController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Sales API";
+    }
+}
