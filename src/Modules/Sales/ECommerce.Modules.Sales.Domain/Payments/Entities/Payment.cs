411cac3742737f2cbd5998d95e71877366b4d01a
kw <k.wojtasinski@wp.pl>
2022-02-23 20:52:01 +0100

    Added user field

--- src/Modules/Sales/ECommerce.Modules.Sales.Domain/Payments/Entities/Payment.cs
+++ src/Modules/Sales/ECommerce.Modules.Sales.Domain/Payments/Entities/Payment.cs
@@ -13,20 +13,22 @@ namespace ECommerce.Modules.Sales.Domain.Payments.Entities
     {
         public string PaymentNumber { get; private set; }
         public Order Order { get; private set; }
+        public Guid UserId { get; private set; }
 
         private Payment() { }
 
-        public Payment(AggregateId id, string paymentNumber, Order order)
+        public Payment(AggregateId id, string paymentNumber, Order order, Guid userId)
         {
             ValidatePayment(paymentNumber);
             Id = id;
             PaymentNumber = paymentNumber;
             Order = order;
+            UserId = userId;
         }
 
-        public static Payment Create(string paymentNumber, Order order)
+        public static Payment Create(string paymentNumber, Order order, Guid userId)
         {
-            var payment = new Payment(Guid.NewGuid(), paymentNumber, order);
+            var payment = new Payment(Guid.NewGuid(), paymentNumber, order, userId);
             return payment;
         }
 
