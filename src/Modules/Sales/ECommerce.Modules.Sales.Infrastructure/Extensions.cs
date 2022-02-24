a11b7a1bca2f88cb69adad1dbb23755480c6b99d
kw <k.wojtasinski@wp.pl>
2022-02-24 22:01:41 +0100

    Registered repositories

--- src/Modules/Sales/ECommerce.Modules.Sales.Infrastructure/Extensions.cs
+++ src/Modules/Sales/ECommerce.Modules.Sales.Infrastructure/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using ECommerce.Modules.Sales.Domain.ItemSales.Repositories;
+using ECommerce.Modules.Sales.Domain.Orders.Repositories;
+using ECommerce.Modules.Sales.Domain.Payments.Repositories;
 using ECommerce.Modules.Sales.Infrastructure.EF;
 using ECommerce.Modules.Sales.Infrastructure.EF.Repositories;
 using ECommerce.Shared.Infrastructure.Postgres;
@@ -10,9 +12,15 @@ namespace ECommerce.Modules.Sales.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            services.AddSingleton<IItemRepository, InMemoryItemRepository>();
-            services.AddSingleton<IItemSaleRepository, InMemoryItemSaleRepository>();
+            //services.AddSingleton<IItemRepository, InMemoryItemRepository>();
+            //services.AddSingleton<IItemSaleRepository, InMemoryItemSaleRepository>();
             services.AddPostgres<SalesDbContext>();
+            services.AddScoped<IItemRepository, ItemRepository>();
+            services.AddScoped<IItemSaleRepository, ItemSaleRepository>();
+            services.AddScoped<IItemCartRepository, ItemCartRepository>();
+            services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
             return services;
         }
     }
