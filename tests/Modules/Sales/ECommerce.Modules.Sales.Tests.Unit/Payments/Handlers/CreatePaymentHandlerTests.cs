a7c725092207ce5ecb3875d3eace5f6a7b720532
kw <k.wojtasinski@wp.pl>
2022-03-03 21:53:08 +0100

    Added some tests to sales module

--- tests/Modules/Sales/ECommerce.Modules.Sales.Tests.Unit/Payments/Handlers/CreatePaymentHandlerTests.cs
+++ tests/Modules/Sales/ECommerce.Modules.Sales.Tests.Unit/Payments/Handlers/CreatePaymentHandlerTests.cs
@@ -1,12 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ECommerce.Modules.Sales.Application.Payments.Commands;
+using ECommerce.Modules.Sales.Application.Payments.Commands.Handlers;
+using ECommerce.Modules.Sales.Application.Payments.Events;
+using ECommerce.Modules.Sales.Application.Payments.Exceptions;
+using ECommerce.Modules.Sales.Domain.Orders.Entities;
+using ECommerce.Modules.Sales.Domain.Orders.Repositories;
+using ECommerce.Modules.Sales.Domain.Payments.Entities;
+using ECommerce.Modules.Sales.Domain.Payments.Repositories;
+using ECommerce.Shared.Abstractions.Contexts;
+using ECommerce.Shared.Abstractions.Messagging;
+using ECommerce.Shared.Abstractions.Time;
+using NSubstitute;
+using Shouldly;
+using Xunit;
 
 namespace ECommerce.Modules.Sales.Tests.Unit.Payments.Handlers
 {
-    internal class CreatePaymentHandlerTests
+    public class CreatePaymentHandlerTests
     {
+        [Fact]
+        public async Task given_invalid_id_should_throw_an_exception()
+        {
+            var command = new CreatePayment(Guid.NewGuid());
+            var expectedException = new OrderNotFoundException(command.OrderId);
+
+            var exception = await Record.ExceptionAsync(() => _handler.HandleAsync(command));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<OrderNotFoundException>();
+            exception.Message.ShouldBe(expectedException.Message);
+        }
+
+        [Fact]
+        public async Task given_valid_id_should_add_payment()
+        {
+            var order = new Order(Guid.NewGuid(), "ORD", 1200M, Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            var command = new CreatePayment(order.Id);
+            _orderRepository.GetAsync(order.Id).Returns(order);
+
+            await _handler.HandleAsync(command);
+
+            await _paymentRepository.Received(1).AddAsync(Arg.Any<Payment>());
+        }
+
+        [Fact]
+        public async Task given_valid_id_should_publish_event_when_add_payment()
+        {
+            var order = new Order(Guid.NewGuid(), "ORD", 1200M, Guid.NewGuid(), Guid.NewGuid(), DateTime.Now);
+            var command = new CreatePayment(order.Id);
+            _orderRepository.GetAsync(order.Id).Returns(order);
+
+            await _handler.HandleAsync(command);
+
+            var @event = new PaymentAdded(command.Id, command.OrderId);
+            await _messageBroker.Received(1).PublishAsync(new IMessage[] { @event });
+        }
+
+        private readonly IPaymentRepository _paymentRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IClock _clock;
+        private readonly IMessageBroker _messageBroker;
+        private readonly IContext _context;
+        private readonly CreatePaymentHandler _handler;
+
+        public CreatePaymentHandlerTests()
+        {
+            _paymentRepository = Substitute.For<IPaymentRepository>();
+            _orderRepository = Substitute.For<IOrderRepository>();
+            _clock = Substitute.For<IClock>();
+            _clock.CurrentDate().Returns(DateTime.UtcNow);
+            _messageBroker = Substitute.For<IMessageBroker>();
+            _context = Substitute.For<IContext>();
+            _handler = new CreatePaymentHandler(_paymentRepository, _orderRepository, _clock, _messageBroker, _context);
+        }
     }
 }
