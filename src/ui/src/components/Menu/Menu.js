cca7f45c084e985c039566aba436c331ccc820ec
kw <k.wojtasinski@wp.pl>
2022-03-17 21:26:04 +0100

    Added menubar

--- src/ui/src/components/Menu/Menu.js
+++ src/ui/src/components/Menu/Menu.js
@@ -1,8 +1,17 @@
+import style from './Menu.module.css';
+
 function Menu() {
     return (
-        <>
-            Menu
-        </>
+        <nav className={`${style.menuContainer} navbar navbar-expand-lg navbar-light bg-light`}>
+            <ul className={style.menu}>
+                <li className={style.menuItem}>
+                    <a className={`${style.menuItem} navbar-brand`} href="#">Home</a>
+                </li>
+                <li className={style.menuItem}>
+                    <a className={`${style.menuItem} navbar-brand`} href="#">Profile</a>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
