bb71a2ba357670c42a71627c43c28d11337dfb0f
kw <k.wojtasinski@wp.pl>
2022-03-17 18:46:05 +0100

    Added component Layout and modified App

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Footer from './components/Footer/Footer';
 
 function App() {
+  const header = (
+    <Header />
+  );
+
+  const menu = (
+    <Menu />
+  )
+
+  const footer = (
+    <Footer />
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout 
+      header = {header}
+      menu = {menu}
+      footer = {footer}
+      />
   );
 }
 
