4f6c4e4cc5041847a6e6bd337b56e5ff98ba2a4a
kw <k.wojtasinski@wp.pl>
2022-03-18 23:53:35 +0100

    Setup YARP

--- src/APIGateway/ECommerce.APIGateway/Program.cs
+++ src/APIGateway/ECommerce.APIGateway/Program.cs
@@ -1,41 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    public static Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddReverseProxy()
+            .LoadFromConfig(builder.Configuration.GetSection("reverseProxy"));
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+        var app = builder.Build();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", context => context.Response.WriteAsync("ECommerce Gateway!"));
+            app.MapReverseProxy();
+        });
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        var task = app.RunAsync();
+        return task;
+    }
 }
