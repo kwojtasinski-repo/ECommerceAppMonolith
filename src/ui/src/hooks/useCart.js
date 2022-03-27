65e76d3087b09e7d9320493bff0ba11dd13d6d83
kw <k.wojtasinski@wp.pl>
2022-03-27 16:04:15 +0200

    modifed cart hook

--- src/ui/src/hooks/useCart.js
+++ src/ui/src/hooks/useCart.js
@@ -1,4 +1,28 @@
+import useAuth from "./useAuth";
+import axios from "../axios-setup";
+
 export default function useCart(props) {
+    const [auth] = useAuth();
+    let cart = null;
+    let addItem = (item) => {};
+    let removeItem = (item) => {};
+
+    if (auth) {
+        [cart, addItem, removeItem] = authCart();
+        const [localItemsCart] = localCart();
+
+        if (localItemsCart.length > 0) {
+            // add all to authCart
+        }
+
+    } else {
+       [cart, addItem, removeItem] = localCart();
+    }
+
+    return [cart, addItem, removeItem];
+};
+
+function localCart() {
     const localCart = JSON.parse(window.localStorage.getItem('item-cart')) ?? [];
 
     const addItem = (item) => {
@@ -12,4 +36,18 @@ export default function useCart(props) {
     }
 
     return [localCart, addItem, removeItem];
-};
+}
+
+function authCart() {
+    const cart = axios.get("/sales-module/cart/me");
+
+    const addItem = (item) => {
+        axios.post("/sales-module/order-items", item);
+    }
+
+    const removeItem = (id) => {
+        axios.delete(`/sales-module/order-items/${id}`);
+    }
+
+    return [cart, addItem, removeItem];
+}
