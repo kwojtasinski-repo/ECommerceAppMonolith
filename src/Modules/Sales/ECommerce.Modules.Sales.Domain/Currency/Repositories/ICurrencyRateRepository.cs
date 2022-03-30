ef1f946c3d0b4c26489b29ddf12e3c4a2fafd729
kw <k.wojtasinski@wp.pl>
2022-03-30 22:25:55 +0200

    Added CurrencyMap entity in sales module

---
+++ src/Modules/Sales/ECommerce.Modules.Sales.Domain/Currency/Repositories/ICurrencyRateRepository.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Modules.Sales.Domain.Currency.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Modules.Sales.Domain.Currency.Repositories
+{
+    public interface ICurrencyRateRepository
+    {
+        Task AddAsync(CurrencyRate currencyRate);
+        Task<bool> ExistsAsync(Guid id);
+        Task<bool> ExistsAsync(string currencyCode, DateOnly createdDate);
+    }
+}
---
+++ src/Modules/Sales/ECommerce.Modules.Sales.Infrastructure/EF/Configurations/CurrencyRateConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Modules.Sales.Domain.Currency.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Modules.Sales.Infrastructure.EF.Configurations
+{
+    internal class CurrencyRateConfiguration : IEntityTypeConfiguration<CurrencyRate>
+    {
+        public void Configure(EntityTypeBuilder<CurrencyRate> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.CurrencyCode).IsRequired().HasMaxLength(3);
+            builder.Property(i => i.RateDate).IsRequired();
+            builder.Property(i => i.Created).IsRequired().HasDefaultValue(DateOnly.FromDateTime(DateTime.UtcNow));
+            builder.Property(i => i.Rate).IsRequired().HasPrecision(14, 4);
+        }
+    }
+}
