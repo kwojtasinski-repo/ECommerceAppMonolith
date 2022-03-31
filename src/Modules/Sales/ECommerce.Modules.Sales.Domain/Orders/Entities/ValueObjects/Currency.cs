867fbd5dc2d96421ba3b8a95b9785699fc63a6a5
kw <k.wojtasinski@wp.pl>
2022-03-31 23:25:44 +0200

    Changed concept of currency value object

--- src/Modules/Sales/ECommerce.Modules.Sales.Domain/Orders/Entities/ValueObjects/Currency.cs
+++ src/Modules/Sales/ECommerce.Modules.Sales.Domain/Orders/Entities/ValueObjects/Currency.cs
@@ -1,26 +1,23 @@
-﻿using System.Globalization;
-
-namespace ECommerce.Modules.Sales.Domain.Orders.Entities.ValueObjects
+﻿namespace ECommerce.Modules.Sales.Domain.Orders.Entities.ValueObjects
 {
     public class Currency : IEquatable<Currency>
     {
         private readonly string _currencyCode;
-        private readonly decimal _rate;
 
-        public Currency(string currencyCode, decimal rate)
+        public Currency(string currencyCode)
         {
             if (string.IsNullOrWhiteSpace(currencyCode))
             {
                 throw new InvalidOperationException("Currency code cannot be null");
             }
 
-            if (rate < 0)
+            var length = currencyCode.Length;
+            if (length != 3)
             {
-                throw new InvalidOperationException($"Rate '{rate}' cannot be negative");
+                throw new InvalidOperationException($"Currency code '{currencyCode}' length '{length}' have to be 3");
             }
 
             _currencyCode = currencyCode;
-            _rate = rate;
         }
 
         public bool Equals(Currency? other)
@@ -31,7 +28,6 @@ namespace ECommerce.Modules.Sales.Domain.Orders.Entities.ValueObjects
         private IEnumerable<object> GetEqualityComponents()
         {
             yield return _currencyCode;
-            yield return _rate;
         }
 
         public override bool Equals(object? obj)
@@ -55,9 +51,7 @@ namespace ECommerce.Modules.Sales.Domain.Orders.Entities.ValueObjects
 
         public override string ToString()
         {
-            var value = _rate / 100;
-            value.ToString("0.00", CultureInfo.CreateSpecificCulture("en-US"));
-            return $"{value} {_currencyCode}";
+            return $"{_currencyCode}";
         }
     }
 }
