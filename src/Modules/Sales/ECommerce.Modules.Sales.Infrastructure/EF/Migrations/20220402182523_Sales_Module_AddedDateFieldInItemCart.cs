1c4d3d9207cbb394265bc0b2f42afd22c979a77b
kw <k.wojtasinski@wp.pl>
2022-04-02 20:27:39 +0200

    Added date field in itemCarts in sales module

---
+++ src/Modules/Sales/ECommerce.Modules.Sales.Infrastructure/EF/Migrations/20220402182523_Sales_Module_AddedDateFieldInItemCart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Modules.Sales.Infrastructure.EF.Migrations
+{
+    public partial class Sales_Module_AddedDateFieldInItemCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                schema: "sales",
+                table: "ItemCarts",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                schema: "sales",
+                table: "ItemCarts");
+        }
+    }
+}
