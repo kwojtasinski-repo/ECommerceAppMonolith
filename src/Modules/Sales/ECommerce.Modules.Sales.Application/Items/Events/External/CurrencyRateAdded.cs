5438a126a22fd11458d671bc5256d03cd574f9ee
kw <k.wojtasinski@wp.pl>
2022-04-03 22:35:46 +0200

    Added events to sales module

---
+++ src/Modules/Sales/ECommerce.Modules.Sales.Application/Items/Events/External/CurrencyRateAdded.cs
@@ -0,0 +1,6 @@
+﻿using ECommerce.Shared.Abstractions.Events;
+
+namespace ECommerce.Modules.Sales.Application.Items.Events.External
+{
+    internal record CurrencyRateAdded(Guid CurrencyRateId, decimal rate, string currencyCode, DateOnly RateDate) : IEvent;
+}
---
+++ src/Modules/Sales/ECommerce.Modules.Sales.Application/Items/Events/External/CurrencyRateUpdated.cs
@@ -0,0 +1,6 @@
+﻿using ECommerce.Shared.Abstractions.Events;
+
+namespace ECommerce.Modules.Sales.Application.Items.Events.External
+{
+    internal record CurrencyRateUpdated(Guid CurrencyRateId, decimal rate) : IEvent;
+}
