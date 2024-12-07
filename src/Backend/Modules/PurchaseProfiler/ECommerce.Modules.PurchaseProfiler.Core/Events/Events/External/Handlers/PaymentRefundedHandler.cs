950bb38df0c533bbb97a5b1455a96448f0deecfd
kw <k.wojtasinski@wp.pl>
2024-12-07 11:49:11 +0100

    [ECommerceApp] PurchaseProfiler: Handle new events

---
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Events/Events/External/Handlers/PaymentRefundedHandler.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Modules.Sales.Application.Items.Events.External;
+using ECommerce.Shared.Abstractions.Events;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace ECommerce.Modules.PurchaseProfiler.Core.Events.Events.External.Handlers
+{
+    internal class PaymentRefundedHandler : IEventHandler<PaymentRefunded>
+    {
+        private readonly ILogger<PaymentRefundedHandler> _logger;
+
+        public PaymentRefundedHandler(ILogger<PaymentRefundedHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task HandleAsync(PaymentRefunded @event)
+        {
+            _logger.LogInformation("Received event: {eventName}, value: {eventValue}", nameof(SignedUp), JsonSerializer.Serialize(@event));
+            return Task.CompletedTask;
+        }
+    }
+}
