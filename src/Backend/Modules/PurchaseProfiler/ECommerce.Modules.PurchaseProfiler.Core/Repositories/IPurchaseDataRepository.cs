1485fd86635d75568883fd466fdfe21577658b9a
kw <k.wojtasinski@wp.pl>
2024-12-10 19:21:40 +0100

    [ECommerceApp] [PurchaseProfiler] Added PurchaseData Repository

---
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Repositories/IPurchaseDataRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Modules.PurchaseProfiler.Core.Entities;
+
+namespace ECommerce.Modules.PurchaseProfiler.Core.Repositories
+{
+    public interface IPurchaseDataRepository
+    {
+        Task<PurchaseData?> GetByKeyAsync(string key);
+        Task<PurchaseData> AddAsync(PurchaseData purchaseData);
+        Task<PurchaseData?> UpdateAsync(PurchaseData purchaseData);
+        Task<bool> DeleteAsync(string key);
+    }
+}
