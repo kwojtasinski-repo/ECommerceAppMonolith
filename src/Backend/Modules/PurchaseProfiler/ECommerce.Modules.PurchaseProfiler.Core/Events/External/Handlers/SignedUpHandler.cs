1dc30bd189e9cacce8862e0000888d2120f4a85b
kw <k.wojtasinski@wp.pl>
2024-12-10 19:57:35 +0100

    [ECommerceApp] [PurchaseProfiler] Used UserRepository

--- src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Events/External/Handlers/SignedUpHandler.cs
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Events/External/Handlers/SignedUpHandler.cs
@@ -1,4 +1,5 @@
-﻿using ECommerce.Modules.Sales.Application.Items.Events.External;
+﻿using ECommerce.Modules.PurchaseProfiler.Core.Repositories;
+using ECommerce.Modules.Sales.Application.Items.Events.External;
 using ECommerce.Shared.Abstractions.Events;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
@@ -8,16 +9,29 @@ namespace ECommerce.Modules.PurchaseProfiler.Core.Events.External.Handlers
     internal sealed class SignedUpHandler : IEventHandler<SignedUp>
     {
         private readonly ILogger<SignedUpHandler> _logger;
+        private readonly IUserRepository _userRepository;
 
-        public SignedUpHandler(ILogger<SignedUpHandler> logger)
+        public SignedUpHandler(ILogger<SignedUpHandler> logger, IUserRepository userRepository)
         {
             _logger = logger;
+            _userRepository = userRepository;
         }
 
-        public Task HandleAsync(SignedUp @event)
+        public async Task HandleAsync(SignedUp @event)
         {
             _logger.LogInformation("Received event: {eventName}, value: {eventValue}", nameof(SignedUp), JsonSerializer.Serialize(@event));
-            return Task.CompletedTask;
+            try
+            {
+                await _userRepository.AddAsync(new Entities.User
+                {
+                    UserId = @event.UserId,
+                    Email = @event.Email,
+                });
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "{handler}: There was an error", nameof(SignedUpHandler));
+            }
         }
     }
 }
