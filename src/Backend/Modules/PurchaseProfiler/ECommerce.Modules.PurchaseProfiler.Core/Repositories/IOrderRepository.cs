a911b8706c292a8dc6b21dc2a1d8bc56bf603dc5
kw <k.wojtasinski@wp.pl>
2024-12-10 19:18:25 +0100

    [ECommerceApp] [PurchaseProfiler] Added Order Repository

---
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Modules.PurchaseProfiler.Core.Entities;
+
+namespace ECommerce.Modules.PurchaseProfiler.Core.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order?> GetByKeyAsync(string key);
+        Task<Order> AddAsync(Order order);
+        Task<Order?> UpdateAsync(Order order);
+        Task<bool> DeleteAsync(string key);
+    }
+}
---
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Repositories/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Modules.PurchaseProfiler.Core.Entities;
+
+namespace ECommerce.Modules.PurchaseProfiler.Core.Repositories
+{
+    internal class OrderRepository(IGenericRepository<Order, long> genericRepository)
+        : IOrderRepository
+    {
+        public async Task<Order> AddAsync(Order order)
+        {
+            return await genericRepository.AddAsync(order);
+        }
+
+        public async Task<bool> DeleteAsync(string key)
+        {
+            return await genericRepository.DeleteAsync(key);
+        }
+
+        public async Task<Order?> GetByKeyAsync(string key)
+        {
+            return await genericRepository.GetByKeyAsync(key);
+        }
+
+        public async Task<Order?> UpdateAsync(Order order)
+        {
+            return await genericRepository.UpdateAsync(order);
+        }
+    }
+}
