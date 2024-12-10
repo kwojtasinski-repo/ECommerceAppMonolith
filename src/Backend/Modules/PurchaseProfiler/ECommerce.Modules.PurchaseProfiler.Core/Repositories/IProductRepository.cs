91f91e70991d433c0f3112dccae07467260c1cef
kw <k.wojtasinski@wp.pl>
2024-12-10 19:20:35 +0100

    [ECommerceApp] [PurchaseProfiler] Added PurchaseData Repository

---
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Modules.PurchaseProfiler.Core.Entities;
+
+namespace ECommerce.Modules.PurchaseProfiler.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByKeyAsync(string key);
+        Task<Product> AddAsync(Product product);
+        Task<Product?> UpdateAsync(Product product);
+        Task<bool> DeleteAsync(string key);
+    }
+}
---
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Core/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Modules.PurchaseProfiler.Core.Entities;
+
+namespace ECommerce.Modules.PurchaseProfiler.Core.Repositories
+{
+    internal class ProductRepository(IGenericRepository<Product, long> genericRepository)
+        : IProductRepository
+    {
+        public async Task<Product> AddAsync(Product product)
+        {
+            return await genericRepository.AddAsync(product);
+        }
+
+        public async Task<bool> DeleteAsync(string key)
+        {
+            return await genericRepository.DeleteAsync(key);
+        }
+
+        public async Task<Product?> GetByKeyAsync(string key)
+        {
+            return await genericRepository.GetByKeyAsync(key);
+        }
+
+        public async Task<Product?> UpdateAsync(Product product)
+        {
+            return await genericRepository.UpdateAsync(product);
+        }
+    }
+}
