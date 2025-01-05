c6e8c094ccbebb4753b0b9c78c0da4125e4cbac0
kw <k.wojtasinski@wp.pl>
2025-01-05 22:01:26 +0100

    [ECommerceApp] Gateway Setup, Secured PurchaseProfiler endpoint

--- src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Api/Controllers/RecomendationsController.cs
+++ src/Backend/Modules/PurchaseProfiler/ECommerce.Modules.PurchaseProfiler.Api/Controllers/RecomendationsController.cs
@@ -1,15 +1,22 @@
 ﻿using ECommerce.Modules.PurchaseProfiler.Core.Services;
+using ECommerce.Shared.Abstractions.Contexts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Modules.PurchaseProfiler.Api.Controllers
 {
-    internal class RecomendationsController(IRecommendationService recommendationService)
+    [Authorize]
+    internal class RecomendationsController
+        (
+            IRecommendationService recommendationService,
+            IContext context
+        )
         : BaseController
     {
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> GetRecommendations(Guid userId)
+        [HttpGet]
+        public async Task<IActionResult> GetRecommendations()
         {
-            var recommendations = await recommendationService.GetRecommendationOnCurrentWeek(userId);
+            var recommendations = await recommendationService.GetRecommendationOnCurrentWeek(context.Identity.Id);
             return Ok(recommendations);
         }
     }
