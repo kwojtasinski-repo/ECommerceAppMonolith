14fc0e57e89be1ada5c4f114f0af77dbc868acbb
kw <k.wojtasinski@wp.pl>
2025-01-06 18:28:53 +0100

    [ECommerceApp] [Frontend] Integrate with backend

--- src/ui/src/components/RecommendCarousel/RecommendCarousel.js
+++ src/ui/src/components/RecommendCarousel/RecommendCarousel.js
@@ -1,33 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Carousel, Button } from "react-bootstrap";
 import styles from "./RecommendCarousel.module.css";
+import ReducerContext from "../../context/ReducerContext";
 
 const RecommendedCarousel = () => {
   const [isVisible, setIsVisible] = useState(true);
-  const recommendedProducts = [
-    {
-      id: 1,
-      name: "Produkt 1",
-      image: "https://via.placeholder.com/100x100",
-      link: "/product/1",
-    },
-    {
-      id: 2,
-      name: "Produkt 2",
-      image: "https://via.placeholder.com/100x100",
-      link: "/product/2",
-    },
-    {
-      id: 3,
-      name: "Produkt 3",
-      image: "https://via.placeholder.com/100x100",
-      link: "/product/3",
-    },
-  ];
+  const context = useContext(ReducerContext);
 
   return (
     <>
-        {recommendedProducts && recommendedProducts.length > 0 ?
+        {context.state.recommendationProducts && context.state.recommendationProducts.length > 0 ?
             <div className="mt-3">
                 <div className="d-flex justify-content-center align-items-center mb-2">
                     <h5 className="mb-0 me-2">Polecane produkty</h5>
@@ -41,17 +23,17 @@ const RecommendedCarousel = () => {
                 </div>
                 {isVisible ?
                     <Carousel data-bs-theme="dark">
-                        {recommendedProducts.map((product) => (
-                        <Carousel.Item key={product.id}>
+                        {context.state.recommendationProducts.map((product) => (
+                        <Carousel.Item key={product.productId}>
                             <div className={`d-flex flex-column align-items-center ${styles.carouselItem}`}>
                             <img
-                                src={product.image}
+                                src={product.imageUrl}
                                 alt={product.name}
                                 style={{ maxWidth: "100px", maxHeight: "100px", borderRadius: "10px" }}
                             />
                             <h6 className="mt-2">{product.name}</h6>
                             <Button
-                                href={product.link}
+                                href={`/items/${product.productSaleId}`}
                                 variant="primary"
                                 size="sm"
                                 className="mt-2"
